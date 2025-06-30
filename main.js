85b85598beedc2954e18de67975a044197970aa6
haytam <haytam@gmail.com>
2025-06-30 12:36:59 +0100

    somme change in main.js

--- main.js
+++ main.js
@@ -3,12 +3,15 @@ const btn = document.getElementById('btn')
 btn.addEventListener('click', function () {
   var error = document.querySelector('.error')
   var email = document.getElementById('text')
-  var regEx = /\w+@\w+\.(com|net|org|net)/gi
+  var regEx = /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/gi
   if (!regEx.test(email.value)) {
     email.classList.add('active')
     error.classList.add('active')
   } else {
-    email.classList.remove('active')
-    error.classList.remove('active')
+  email.classList.remove('active') ;
+  error.classList.remove('active') ;
+  alert('Thank you! You will be notified.') ;
+  email.value = '' ;
+
   }
 })
